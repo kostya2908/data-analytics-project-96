7d0e7709940061765d1f3ddcb404da764560a483
kostya2908 <k.mokievets@gmail.com>
2024-01-21 18:21:56 +0300

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,27 +1,29 @@
 WITH tab AS (
-	SELECT DISTINCT ON (visitor_id)
-		visitor_id,
-		DENSE_RANK() OVER (ORDER BY visitor_id),
-		visit_date,
-		source,
-		medium,
-		campaign
-	FROM sessions
-	WHERE medium != 'organic'
-	ORDER BY 1,3 DESC
+    SELECT DISTINCT ON (visitor_id)
+        visitor_id,
+        visit_date,
+        source,
+        medium,
+        campaign
+    FROM sessions
+    WHERE medium != 'organic'
+    ORDER BY 1, 2 DESC
 )
+
 SELECT
-	visitor_id,
-	visit_date,
-	source AS utm_source,
-	medium AS utm_medium,
-	campaign AS utm_campaign,
-	lead_id,
-	created_at,
-	amount,
-	closing_reason,
-	status_id		
+    tab.visitor_id,
+    visit_date,
+    source AS utm_source,
+    medium AS utm_medium,
+    campaign AS utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
 FROM tab
-LEFT JOIN leads USING (visitor_id)
+LEFT JOIN leads
+    ON
+        tab.visitor_id = leads.visitor_id
 WHERE visit_date <= created_at
 ORDER BY 8 DESC NULLS LAST, 2, 3 LIMIT 10;
