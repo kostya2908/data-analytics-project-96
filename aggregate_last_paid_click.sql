3a2708dc7da27aa3cfa29f8018805835c523cfe5
kostya2908 <k.mokievets@gmail.com>
2024-01-22 01:49:04 +0300

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -3,31 +3,32 @@ DROP VIEW paid_visits CASCADE;
 CREATE VIEW paid_visits AS (
     SELECT DISTINCT ON (visitor_id)
         visitor_id,
-        visit_date,
-        source AS utm_source,
-        medium AS utm_medium,
-        campaign AS utm_campaign
+        visit_date
     FROM sessions
     WHERE medium != 'organic'
     ORDER BY 1, 2 DESC
 );
 CREATE VIEW step_2 AS (
     SELECT
-        visitor_id,
-        visit_date,
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        lead_id,
-        created_at,
-        amount,
-        closing_reason,
-        status_id
-    FROM paid_visits
-    LEFT JOIN leads
+        pv.visitor_id,
+        pv.visit_date,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id
+    FROM paid_visits AS pv
+    LEFT JOIN sessions AS s
         ON
-            paid_visits.visitor_id = leads.visitor_id
-    WHERE visit_date <= created_at
+            pv.visitor_id = s.visitor_id
+            AND pv.visit_date = s.visit_date
+    LEFT JOIN leads AS l
+        ON
+            pv.visitor_id = l.visitor_id
+    WHERE pv.visit_date <= l.created_at
     ORDER BY 8 DESC NULLS LAST, 2, 3-- LIMIT 10
 );
 --STEP_3--
@@ -60,31 +61,31 @@ CREATE VIEW ya_date_spent AS (
 SELECT
     DATE(pv.visit_date) AS visit_date,
     COUNT(pv.visitor_id) AS visitors_count,
-    pv.utm_source,
-    pv.utm_medium,
-    pv.utm_campaign,
+    s2.utm_source,
+    s2.utm_medium,
+    s2.utm_campaign,
     CASE
         WHEN vds.sum_vk IS NULL THEN yds.sum_ya
         WHEN yds.sum_ya IS NULL THEN vds.sum_vk
     END AS total_cost,
     COUNT(s2.lead_id) AS leads_count,
-    COUNT(s2.lead_id) FILTER (WHERE status_id = 142) AS purchases_count,
-    SUM(amount) AS revenue
+    COUNT(s2.lead_id) FILTER (WHERE s2.status_id = 142) AS purchases_count,
+    SUM(s2.amount) AS revenue
 FROM paid_visits AS pv
-LEFT JOIN vk_date_spent AS vds
-    ON
-        vds.day_vk = DATE(pv.visit_date)
-        AND vds.utm_source = pv.utm_source
-        AND vds.utm_medium = pv.utm_medium
-        AND vds.utm_campaign = pv.utm_campaign
-LEFT JOIN ya_date_spent AS yds
-    ON
-        yds.day_ya = DATE(pv.visit_date)
-        AND yds.utm_source = pv.utm_source
-        AND yds.utm_medium = pv.utm_medium
-        AND yds.utm_campaign = pv.utm_campaign
 LEFT JOIN step_2 AS s2
     ON
         pv.visitor_id = s2.visitor_id
+LEFT JOIN vk_date_spent AS vds
+    ON
+        vds.day_vk = DATE(pv.visit_date)
+        AND vds.utm_source = s2.utm_source
+        AND vds.utm_medium = s2.utm_medium
+        AND vds.utm_campaign = s2.utm_campaign
+LEFT JOIN ya_date_spent AS yds
+    ON
+        yds.day_ya = DATE(pv.visit_date)
+        AND yds.utm_source = s2.utm_source
+        AND yds.utm_medium = s2.utm_medium
+        AND yds.utm_campaign = s2.utm_campaign
 GROUP BY 1, 3, 4, 5, 6
 ORDER BY 9 DESC NULLS LAST, 1, 2, 3, 4, 5 LIMIT 15;
